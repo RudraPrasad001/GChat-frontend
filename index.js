c02c2a77c03e25747c29e4906badefb38bf219c7
RudraPrasad001 <rudraprasad.email@gmail.com>
2025-06-18 19:22:53 +0530

    Basic Socket

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  }
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "HELLP" });
+});
+
+io.on('connection', (socket) => {
+  console.log("Socket connected:", socket.id);
+
+  socket.on('message', ({text,id}) => {
+    console.log('Received message:', text);
+    socket.broadcast.emit('message',{text,id});
+  });
+
+  socket.on('disconnect', () => {
+    console.log("Socket disconnected:", socket.id);
+  });
+});
+
+server.listen(3000, () => console.log("🚀 Server listening on port 3000"));
